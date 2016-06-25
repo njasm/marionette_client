091170fb55b2b348d613c406acfa230148e88bab
Nelson J Morais <njmorais@proactive-hosters.com>
2016-06-25 12:13:18 +0100

    finished navigater interface definition

--- navigation.go
+++ navigation.go
@@ -2,6 +2,12 @@ package marionette_client
 
 type Navigater interface {
 	Get(url string) (*response, error)
+	GetPageSource() (*response, error)
+	GetTitle() (string, error)
+	CurrentUrl() error
+	Refresh() error
+	Back() error
+	Forward() error
 }
 
 type navigation struct {
