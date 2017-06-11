61ad2a5357c49b2f5ce1bac2fa9dd7a73e4d98a8
Nelson J Morais <njmorais@proactive-hosters.com>
2017-06-11 10:29:56 +0100

    fixed Capabilities struct specificationLevel data type for ff 53.0.3

--- capabilities.go
+++ capabilities.go
@@ -5,7 +5,7 @@ type Capabilities struct {
 	BrowserVersion                string
 	PlatformName                  string
 	PlatformVersion               string
-	SpecificationLevel            string
+	SpecificationLevel            uint
 	RaisesAccessibilityExceptions bool
 	Rotatable                     bool
 	AcceptSslCerts                bool
@@ -19,3 +19,24 @@ type Capabilities struct {
 	Version                       string
 	Command_id                    uint32
 }
+
+/*
+{"capabilities":
+  {
+	"browserName":"firefox",
+	"browserVersion":"53.0.3","platformName":"linux",
+	"platformVersion":"4.8.12-040812-generic",
+	"pageLoadStrategy":"normal",
+	"acceptInsecureCerts":false,
+	"timeouts":{
+		"implicit":0,
+		"pageLoad":300000,
+		"script":30000
+	},
+	"rotatable":false,
+	"specificationLevel":0,
+	"moz:processID":2004,
+	"moz:profile":"/home/travis/.mozilla/firefox/594k4686.default",
+	"moz:accessibilityChecks":false
+  }
+}**/
